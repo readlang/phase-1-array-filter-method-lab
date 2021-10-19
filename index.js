c62969b9f9bc3ca1613daf53a21752699832efd5
Read Langworthy <rlangworthy@gmail.com>
2021-10-19 11:26:35 -0700

    commit tree dc6621f37553cef921a1e91830165cca083f3c02

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Code your solution here
+
+const { stringContaining } = require("expect")
+
+
+
+function findMatching(arrayOfDrivers, name) {
+    return(
+    arrayOfDrivers.filter(function (element) {
+        return( element.toUpperCase() == name.toUpperCase() )
+    })
+    )
+}
+
+
+
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+// console.log( "First print: " +
+// drivers.filter(function (member) {
+//     return (member === "Bobby")
+// }) 
+// )
+
+// console.log( 
+//     findMatching(drivers, 'Bobby')
+// )
+
+
+function fuzzyMatch(arrayOfDrivers, letters) {
+    return(
+
+        arrayOfDrivers.filter(function (element) {
+            return( element.slice(0, letters.length) === letters )
+        }
+        )
+    )
+}
+
+
+// console.log(
+// fuzzyMatch(drivers, "Sa")
+// )
+
+function matchName(arrayOfObjs, nameString) {
+    return(
+        arrayOfObjs.filter(function (element) {
+            return( element.name === nameString)
+        })
+    )
+}
+
